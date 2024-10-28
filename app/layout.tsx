9d1bd44dfee0e5f492c6952dc0afec94e82257c4
cpmoore0317 <cpmoore317@gmail.com>
2024-10-28 17:51:32 -0500

    Fixed header color

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }: Props) {
     <html lang="en">
       <body className="antialiased bg-[#00003c] text-white">
         {/* Header */}
-        <header className="flex justify-between items-center px-6 py-4 bg-[#1ED2AF] text-[#00003c]">
+        <header className="flex justify-between items-center px-6 py-4 bg-[#54F4D0] text-[#00003c]">
           <h1 className="font-bold">Cinema Guru</h1>
           <div className="flex items-center space-x-4">
             <span>Welcome, user@email.com</span>
