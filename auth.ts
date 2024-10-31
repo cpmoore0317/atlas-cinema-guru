5bef7e3ab124d973ba4e9dfcb75e90f5a9fa91a9
cpmoore0317 <cpmoore317@gmail.com>
2024-10-31 11:22:17 -0500

    auth.ts changes for login logic

--- auth.ts
+++ auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   theme: {
@@ -6,7 +7,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
-  providers: [],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.AUTH_GITHUB_ID, // Your GitHub client ID
+      clientSecret: process.env.AUTH_GITHUB_SECRET, // Your GitHub client secret
+    }),
+  ],
   callbacks: {
     authorized: async ({ auth }) => {
       // Logged in users are authenticated, otherwise redirect to login page
