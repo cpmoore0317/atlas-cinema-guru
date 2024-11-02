d4b5d0087b8cf165e4e2cfe837eba631301dc1b7
cpmoore0317 <cpmoore317@gmail.com>
2024-11-02 16:30:22 -0500

    Test

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ import { signIn } from "next-auth/react";
 export default function LoginPage() {
   return (
     <div className="flex items-center justify-center h-screen bg-[#00003c] text-white">
-      <h1 className="text-2xl mb-4">Login to Cinema Guru</h1>
+      <h1 className="text-2xl mb-4">Login</h1>
       <button
         onClick={() => signIn("github")}
         className="px-4 py-2 bg-[#54F4D0] text-[#00003c] rounded hover:bg-[#1ED2AF]"
