cf0dcba1df39802d41a04dba63102615a4dd4b71
cpmoore0317 <cpmoore317@gmail.com>
2024-11-05 17:50:34 -0600

    Updated auth routing

--- auth.ts
+++ auth.ts
@@ -1,22 +1,19 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: '/login',
+  },
   theme: {
     brandColor: "#1ED2AF",
     logo: "/logo.png",
     buttonText: "#ffffff",
   },
   providers: [
-    GitHubProvider({
-      clientId: process.env.AUTH_GITHUB_ID, // Your GitHub client ID
-      clientSecret: process.env.AUTH_GITHUB_SECRET, // Your GitHub client secret
-    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    })
   ],
-  callbacks: {
-    authorized: async ({ auth }) => {
-      // Logged in users are authenticated, otherwise redirect to login page
-      return !!auth;
-    },
-  },
 });
--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,5 @@
-import { NextResponse, NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
-
-export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
-
-  // If the user is not authenticated and trying to access a protected route
-  if (!token && req.nextUrl.pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', req.url));
-  }
-
-  // Allow requests if user is authenticated or if they are going to the login page
-  return NextResponse.next();
-}
+export { auth as middleware } from "@/auth";
 
 export const config = {
-  matcher: [
-    // Protect all routes except the login page
-    '/((?!api|_next|static|login).*)',
-  ],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|logo.png).*)"],
 };
