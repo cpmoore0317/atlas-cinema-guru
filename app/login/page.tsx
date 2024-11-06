006b25bd9a1fe345cda96c07f73ef326ca2028fa
Parker Moore <126629203+cpmoore0317@users.noreply.github.com>
2024-11-06 12:32:48 -0600

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,38 @@
-// app/login/page.tsx
-"use client"; // This makes the component a Client Component
+"use client";
 
-import { signIn } from "next-auth/react";
+import { signIn} from "next-auth/react";
+import Image from "next/image";
 
 export default function LoginPage() {
+
   return (
-    <div className="flex items-center justify-center h-screen bg-[#00003c] text-white">
-      <button
-        onClick={() => signIn("github")}
-        className="px-4 py-2 bg-[#54F4D0] text-[#00003c] rounded hover:bg-[#1ED2AF]"
+    <div className="p-5 bg-white shadow-md rounded-2xl flex flex-col items-center">
+      <Image src="/logo.png" alt="Logo" className="mb-4" width={150} height={150} />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
       >
-        Sign in with GitHub
-      </button>
+        <button
+          type="button"
+          onClick={() => signIn("github", { callbackUrl: "/home" })}
+          className="flex mt-4 bg-white border border-[gray] text-[#000000] rounded-lg py-2 px-4 hover:bg-[#1ED2AF] hover:text-white transition duration-200 group"
+        >
+          <div className="flex justify-between gap-20">
+            Sign in with GitHub
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+              className="fill-current text-[#000000] group-hover:text-white transition duration-200"
+            >
+              <title>github</title>
+              <path d="M12,2A10,10,0,0,0,8.84,21.5c.5.08.66-.23.66-.5V19.31C6.73,19.91,6.14,18,6.14,18A2.69,2.69,0,0,0,5,16.5c-.91-.62.07-.6.07-.6a2.1,2.1,0,0,1,1.53,1,2.15,2.15,0,0,0,2.91.83,2.16,2.16,0,0,1,.63-1.34C8,16.17,5.62,15.31,5.62,11.5a3.87,3.87,0,0,1,1-2.71,3.58,3.58,0,0,1,.1-2.64s.84-.27,2.75,1a9.63,9.63,0,0,1,5,0c1.91-1.29,2.75-1,2.75-1a3.58,3.58,0,0,1,.1,2.64,3.87,3.87,0,0,1,1,2.71c0,3.82-2.34,4.66-4.57,4.91a2.39,2.39,0,0,1,.69,1.85V21c0,.27.16.59.67.5A10,10,0,0,0,12,2Z" />
+            </svg>
+          </div>
+        </button>
+      </form>
     </div>
   );
 }
