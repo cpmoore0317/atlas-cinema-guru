94a0b8e46f6f564e30059e44ffec67b603f47436
Parker Moore <126629203+cpmoore0317@users.noreply.github.com>
2024-11-06 12:29:39 -0600

    Create SearchHandler.tsx

---
+++ app/components/SearchHandler.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import SearchBar from "./SearchBar";
+
+interface SearchHandlerProps {
+  onSearch: (query: string) => void;
+}
+
+export default function SearchHandler({ onSearch }: SearchHandlerProps) {
+  const handleSearch = async (query: string) => {
+    onSearch(query);
+  };
+
+  return (
+    <SearchBar onSearch={handleSearch} />
+  );
+}
