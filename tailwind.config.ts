577827c27e5007e3ff98b481d1feaf62b26da222
cpmoore0317 <cpmoore317@gmail.com>
2024-11-06 14:15:19 -0600

    Updated configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'header-icon': '#00003c',
-        'sidebar-icon': '#ffffff',
+        placeholderGrey: '#b0b0b0',
+        Teal: '#1ED2AF',
+        greenTeal: '#20a48e',
+        darkBlue: '#00003c',
       },
     },
   },
